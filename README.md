d632b7597d3ebd85d7b1f2be99645d1e8ec45838
Freewind <nowindlee@gmail.com>
2019-06-05 00:28:15 +0800

    demo

--- README.md
+++ README.md
@@ -1,11 +1,7 @@
-TypeScript Cypress Route Match Request Data Demo
+TypeScript Cypress Check Request Body Demo
 ================================================
 
-Cypress的功能比较弱，在mock response的时候，只能简单的比较url和method，拿不到request的其它信息来决定返回哪个response
-
-在某些场景下，我们需要根据request提供的某些参数来返回不同的response，来验证逻辑，目前还是实现不了。
-
-https://github.com/cypress-io/cypress/issues/521
+如何利用`cy.route`来检查request的body
 
 ```
 npm install
--- cypress/integration/hello_spec.ts
+++ cypress/integration/hello_spec.ts
@@ -1,33 +1,21 @@
 describe('cypress', () => {
 
-  it('match request data', () => {
+  it('check request body', () => {
 
     cy.server();
-
-    // FIXME
-    // how to get request and check it's 'headers' to respond different stub responses?
-
-    // cy.route('GET', '/public/data.json', (routeData: Cypress.RouteOptions, x:any) => {
-    //   console.log('### routeData', routeData, x);
-    //   return {
-    //     data: {"message":"aaa"}
-    //   }
-    // })
-    
-    // cy.route({
-    //   method: 'GET',
-    //   url: '/public/data.json',
-    //   onRequest: (...args): void => {
-    //     console.log('### onRequest', args)
-    //   },
-    //   onResponse: (...args): void => {
-    //     console.log('### onResponse', args)
-    //   }
-    // })
+    cy.route({
+      method: 'POST',
+      url: '/public/data.json',
+      onRequest: (xhr: any): void => {
+        console.log('### onRequest', xhr)
+        expect(xhr.request.body).to.deep.equal({
+          name: 'cypress'
+        })
+      }
+    })
     cy.visit('/public/index.html');
 
-    cy.get('#button1').click();
-    cy.get('#message').should('have.text', 'xxx')
+    cy.get('#button').click();
 
   })
 
